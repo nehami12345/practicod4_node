2f70399ed67346170475013b7b25cea933925205
heineman <b0527676318@gmail.com>
2025-12-21 11:10:07 +0200

    complete

--- server.js
+++ server.js
@@ -6,23 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const RENDER_API_KEY = process.env.RENDER_API_KEY;
 
-
-// app.get('/', async (req, res) => {
-//   try {
-//     const response = await axios.get('https://api.render.com/v1/services', {
-//       headers: {
-//         'Authorization': `Bearer ${RENDER_API_KEY}`,
-//         'Accept': 'application/json'
-//       }
-//     });
-//     res.json(response.data);
-//   } catch (error) {
-//     res.status(500).json({
-//       error: 'Failed to fetch services',
-//       details: error.message
-//     });
-//   }
-// });
 app.get('/', async (req, res) => {
   try {
     const response = await axios.get('https://api.render.com/v1/services', {
@@ -33,9 +16,9 @@ app.get('/', async (req, res) => {
     });
     res.json(response.data);
   } catch (error) {
-    res.status(500).json({ 
+    res.status(500).json({
       error: 'Failed to fetch services',
-      details: error.message 
+      details: error.message
     });
   }
 });
